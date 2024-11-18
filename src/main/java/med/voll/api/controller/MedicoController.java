0c5e97fdd56f0aa74dbd61be57edbb13a8aa06f7
tgoarrais <t.arrais@outlook.com>
2024-11-17 23:57:25 -0300

    Feat: atualização de projeto.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -10,7 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("medicos")
--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -1,6 +1,6 @@
 package med.voll.api.medico;
 
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 import med.voll.api.endereco.Endereco;
